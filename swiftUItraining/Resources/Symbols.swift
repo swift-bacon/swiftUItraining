bf9f0ebf679d6e34c005e841c53ae043f2b9417b
Marcin Pietrzak <pietrzakmarcin@hotmail.com>
2025-08-07 21:52:33 +0200

    add resources

---
+++ swiftUItraining/Resources/Symbols.swift
@@ -0,0 +1,13 @@
+//
+//  Symbols.swift
+//  swiftUItraining
+//
+//  Created by Unit27 on 07/08/2025.
+//
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ swiftUItraining/Resources/Theme.swift
@@ -0,0 +1,14 @@
+//
+//  Theme.swift
+//  swiftUItraining
+//
+//  Created by Unit27 on 07/08/2025.
+//
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
